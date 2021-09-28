35cd44134a29bdd540c15b708b490e5ffc1caa00
brunaopdejesus <bruna.oliveira.656667@gmail.com>
2021-09-28 11:28:13 -0300

    slide

--- galeria.js
+++ galeria.js
@@ -2,32 +2,61 @@
 
 // vem de um banco de dados
 const imagens = [
-    "./img/img10.PNG",
+    "./img/img1.PNG",
     "./img/img2.PNG",
-    "./img/img9.PNG",
+    "./img/img3.PNG",
     "./img/img4.PNG",
     "./img/img5.PNG",
     "./img/img6.PNG",
     "./img/img7.PNG",
     "./img/img8.PNG"
-]
+];
+
+// barra separa os elementos
+const limparId = (urlImagem) => urlImagem.split('/')[2]
+                                            .split('.')[0]
+                                            .replace(" ","-")
+                                            .toLowerCase();
 
 const criarItem = (urlImagem) => {
     const container = document.querySelector(".galeria-container");
-
     const novoLink = document.createElement("a");
-    novoLink.href = urlImagem;
+    novoLink.href = `#${limparId(urlImagem)}`;
     novoLink.classList.add("galeria-itens");
     novoLink.innerHTML = `<img src="${urlImagem}">`;
     container.appendChild(novoLink);
 
-    // container.innerHTML += `
-    //     <a href="" class="galeria-itens">
-    //         <img src="${urlImagem}">
-    //     </a>
-    // `
 }
 
 const carregarImagens = () => imagens.forEach(criarItem);
 
+
+const criarSlide = (urlImagem, indice, arr) => {
+    const container = document.querySelector('.slide-container');
+    const slide = document.createElement('div');
+    slide.classList.add('slide');
+    slide.id = limparId(urlImagem);
+
+    const indiceAnterior = indice > 0 ? indice - 1 : arr.length - 1;    
+    const idAnterior = limparId(arr[indiceAnterior]);
+
+    const indiceProximo = indice < arr.length - 1 ? indice + 1 : 0;
+    const idProximo = limparId(arr[indiceProximo]);
+
+
+    slide.innerHTML = `
+        <div class="imagem-container">
+            <a href="#" class="fechar">&#10006;</a>
+            <a href="#${idAnterior}" class="navegacao anterior">&#171;</a>
+            <img src="${urlImagem}" alt="">
+            <a href="#${idProximo}" class="navegacao proximo">&#187;</a>
+        </div>
+    `
+    container.appendChild(slide);
+}
+
+
+const carregarSlides = () => imagens.forEach(criarSlide);
+
 carregarImagens();
+carregarSlides();
